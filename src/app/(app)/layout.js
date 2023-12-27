04cf7fe106223092875053d9710dc09821e3eee2
rameezqazi23 <rameez.qazi23@gmail.com>
2023-12-28 01:49:58 +0500

    Working on account page, create sidebar display account info and create navigation

--- src/app/(app)/layout.js
+++ src/app/(app)/layout.js
@@ -1,7 +1,15 @@
 import Navbar from '@/app/Components/Navbar'
 import { Inter } from 'next/font/google'
 import '../globals.css'
-// import Navbar from '../Components/Navbar'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../api/auth/[...nextauth]/route'
+import Image from 'next/image';
+import Link from "next/link";
+import { FcSettings } from 'react-icons/fc'
+import { MdAnalytics } from "react-icons/md";
+import LogoutButton from '../Components/buttons/LogoutButton'
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,13 +18,31 @@ export const metadata = {
   description: 'Organize Your links in one place',
 }
 
-export default function AppLayout({ children }) {
+export default async function AppLayout({ children }) {
+
+  const session = await getServerSession(authOptions)
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main>
+        <main className='flex min-h-screen'>
+          <aside className='bg-[#2c2f32] text-gray-300 w-48 p-4'>
+            <div className='w-24 mx-auto'>
+              <Image className='rounded-full cursor-pointer hover:w-24 duration-200' src={session?.user?.image} width={80} height={80} alt='avatar' />
+            </div>
+            <nav className='flex flex-col justify-center items-center mt-8 gap-4'>
+              <Link className='flex justify-center items-center gap-2' href={'/account'}>
+                <FcSettings className='hover:rotate-180 duration-200' size={22} />
+                Setting
+              </Link>
+              <Link className='flex justify-center items-center gap-2' href={'/analytics'}>
+                <MdAnalytics size={22} />
+                Analytics
+              </Link>
+              <LogoutButton />
+            </nav>
+          </aside>
           <div className='max-w-4xl mx-auto px-8'>
-
             {children}
           </div>
         </main>
--- src/app/Components/buttons/LogoutButton.jsx
+++ src/app/Components/buttons/LogoutButton.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { signOut } from "next-auth/react";
 import { TbLogout } from "react-icons/tb";
 
-
 const LogoutButton = () => {
   return (
     <div>
@@ -11,10 +10,10 @@ const LogoutButton = () => {
         onClick={() => {
           signOut();
         }}
-        className="hover:text-red-500 delay-200 flex items-center gap-2"
+        className="hover:text-red-500 duration-200 flex items-center gap-2"
       >
-        Logout <TbLogout size={18} />
-
+        <TbLogout size={22} />
+        Logout
       </button>
     </div>
   );
